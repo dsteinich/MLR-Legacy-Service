68aa2649d32bb2ea19e48ea0e30a558fe4e618e8
Steinich, Dave R. <drsteini@usgs.gov>
2017-09-22 11:26:34 -0500

    line in sand

--- src/main/java/gov/usgs/wma/mlrlegacy/WebSecurityConfig.java
+++ src/main/java/gov/usgs/wma/mlrlegacy/WebSecurityConfig.java
@@ -1,46 +1,89 @@
 package gov.usgs.wma.mlrlegacy;
 
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakAuthenticationProcessingFilter;
+import org.keycloak.adapters.springsecurity.filter.KeycloakPreAuthActionsFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.authentication.preauth.x509.X509AuthenticationFilter;
+import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
-	@Value("${mlrLegacyServicePassword}")
-	private String pwd;
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(keycloakAuthenticationProvider());
+	}
+
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new NullAuthenticatedSessionStrategy();
+	}
+
+	@Bean
+	public FilterRegistrationBean keycloakAuthenticationProcessingFilterRegistrationBean(
+			KeycloakAuthenticationProcessingFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
+
+	@Bean
+	public FilterRegistrationBean keycloakPreAuthActionsFilterRegistrationBean(
+			KeycloakPreAuthActionsFilter filter) {
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(filter);
+		registrationBean.setEnabled(false);
+		return registrationBean;
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
-				.antMatchers("/monitoringLocations/**").permitAll()
-				.antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
-				.antMatchers("/health/**").permitAll()
-				.anyRequest().fullyAuthenticated()
-			.and()
-				.formLogin().defaultSuccessUrl("/swagger-ui.html", true)
-			.and()
-				.logout().logoutSuccessUrl("/swagger-ui.html")
-			.and()
-				.formLogin().permitAll()
-			.and()
-				.logout().permitAll()
-			.and()
-				.csrf().disable()
-		;
+//			.authorizeRequests()
+//				.antMatchers("/monitoringLocations/**").permitAll()
+//				.antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/**").permitAll()
+//				.antMatchers("/health/**").permitAll()
+//				.anyRequest().fullyAuthenticated()
+//			.and()
+//				.formLogin().defaultSuccessUrl("/swagger-ui.html", true)
+//			.and()
+//				.logout().logoutSuccessUrl("/swagger-ui.html")
+//			.and()
+//				.formLogin().permitAll()
+//			.and()
+//				.logout().permitAll()
+//			.and()
+//				.csrf().disable()
+//				.cors()
+//			.and()
+//				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionAuthenticationStrategy(sessionAuthenticationStrategy())
+                .and()
+                .addFilterBefore(keycloakPreAuthActionsFilter(), LogoutFilter.class)
+                .addFilterBefore(keycloakAuthenticationProcessingFilter(), X509AuthenticationFilter.class)
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPoint())
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .anyRequest().permitAll()
+                ;
 	}
 
-	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth
-		.inMemoryAuthentication()
-		.withUser("user").password(pwd).roles("ACTUATOR");
-	}
+//	@Bean
+//	public KeycloakConfigResolver KeycloakConfigResolver() {
+//		return new KeycloakSpringBootConfigResolver();
+//	}
 
 }
